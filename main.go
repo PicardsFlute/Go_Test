fa544bbfd7ed55e366b49c67e1cee9b57e140e73
benjammin12 <benxerri@gmail.com>
2017-10-21 15:39:52 -0400

    Added login form

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 	"html/template"
 	"os"
 	"github.com/gorilla/mux"
-	"strconv"
+	//"strconv"
+	"fmt"
 )
 
 var (
@@ -14,8 +15,8 @@ var (
 )
 
 type Person struct {
-	Name string
-	Age int
+	Email string
+	Password string
 }
 
 func main() {
@@ -25,24 +26,41 @@ func main() {
 	routes.PathPrefix("/public").Handler(http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
 
 	routes.HandleFunc("/",index)
-	routes.HandleFunc("/about/{number}", about)
-	/*
-	server := http.Server{
-		Addr: ":" + os.Getenv("PORT"),
-	}
-	server.ListenAndServe()
-	*/
-	//http.ListenAndServe(os.Getenv("PORT"), nil)
+	//routes.HandleFunc("/about/{number}", about)
+	routes.HandleFunc("/loginPage", loginPage)
+	routes.HandleFunc("/login", loginUser)
+
+
+	// USED FOR HEROKU
 	http.ListenAndServe(":" + os.Getenv("PORT"), routes)
+
+	//USED FOR LOCAL, only use one
+	//http.ListenAndServe(":8080", routes)
 }
 
 
 func index(w http.ResponseWriter, r *http.Request){
 
-	tpl.ExecuteTemplate(w, "index.html", nil)
+	tpl.ExecuteTemplate(w, "Base", nil)
 
 }
 
+func loginPage(w http.ResponseWriter, r *http.Request){
+	tpl.ExecuteTemplate(w,"login",nil)
+}
+
+func loginUser(w http.ResponseWriter, r *http.Request){
+
+	userEmail := r.FormValue("email")
+	userPassword :=	r.FormValue("password")
+	p := Person{userEmail,userPassword}
+	fmt.Println("Email: ", userEmail)
+	fmt.Println("Password: ", userPassword)
+	tpl.ExecuteTemplate(w,"login",p)
+}
+
+
+/*
 func about(w http.ResponseWriter, r *http.Request){
 	vars := mux.Vars(r) //returns a mapping responses
 	personId := vars["number"] //get map with key id number
@@ -56,3 +74,14 @@ func about(w http.ResponseWriter, r *http.Request){
 		tpl.ExecuteTemplate(w, "index.html", p)
 	}
 }
+
+func generateHTML(writer http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("templates/%s.public", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(writer, "layout", data)
+}
+*/
